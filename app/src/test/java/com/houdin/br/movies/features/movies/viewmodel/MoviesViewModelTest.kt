efc07c47c3184db9b5f27ea0bbfbae4915f0f783
Matheus <matheus.gomes@uds.com.br>
2021-06-08 08:19:48 -0300

    Starting movies view model unit testing

---
+++ app/src/test/java/com/houdin/br/movies/features/movies/viewmodel/MoviesViewModelTest.kt
@@ -0,0 +1,12 @@
+package com.houdin.br.movies.features.movies.viewmodel
+
+import com.houdin.br.movies.UnitTest
+
+/**
+ * @author Matheus Gomes on 08/06/2021.
+ */
+class MoviesViewModelTest : UnitTest() {
+    override fun initialize() {
+        TODO("Not yet implemented")
+    }
+}
