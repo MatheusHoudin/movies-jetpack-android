3c679e00c2234d285ddc3553be975f21d16e65eb
Francisco Matheus do Carmo Gomes <matheuscarmo@MMB021333CPS.local>
2021-07-26 16:41:10 -0300

    Creating background

--- app/src/main/java/com/houdin/br/movies/features/moviedetails/presentation/MovieDetailsFragment.kt
+++ app/src/main/java/com/houdin/br/movies/features/moviedetails/presentation/MovieDetailsFragment.kt
@@ -3,15 +3,21 @@ package com.houdin.br.movies.features.moviedetails.presentation
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.houdin.br.movies.R
 import com.houdin.br.movies.databinding.MovieDetailsLayoutBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MovieDetailsFragment : Fragment() {
+class MovieDetailsFragment : BottomSheetDialogFragment() {
 
     private val args: MovieDetailsFragmentArgs by navArgs()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, R.style.CustomBottomSheetDialogTheme)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
